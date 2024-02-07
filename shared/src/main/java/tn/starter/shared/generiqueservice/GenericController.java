60472a592545ab8de6e48fbc90bbc09655b6cc06
AtefMaddouri <maddouriatef@gmail.com>
2024-02-07 10:02:08 +0100

    [TEST] update keycloak version

--- shared/src/main/java/tn/starter/shared/generiqueservice/GenericController.java
+++ shared/src/main/java/tn/starter/shared/generiqueservice/GenericController.java
@@ -1,8 +1,11 @@
 package tn.starter.shared.generiqueservice;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,18 +15,33 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-public class GenericController<T,ID> {
+public class GenericController<DTO, T,ID> {
 	@Autowired
 	IGenericService<T,ID> genericService;
 
+	ModelMapper modelMapper = new ModelMapper();
+
 	@PostMapping
-	public T add(@RequestBody T dto) {
-		return genericService.add(dto);
+	public DTO add(@RequestBody DTO dto) {
+
+		T entity = (T) new Object();
+		BeanUtils.copyProperties(dto,entity);
+		genericService.add(entity);
+		BeanUtils.copyProperties(entity,dto);
+		return dto;
 	}
 
-	@PutMapping()
-	public T update(@RequestBody T dto) {
-		return	genericService.update(dto);
+	@PutMapping
+	public DTO update(@RequestBody DTO dto) {
+		Class<T> entityClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[1];
+		T entity = modelMapper.map(dto , entityClass);
+		BeanUtils.copyProperties(dto,entity);
+
+		genericService.update(entity);
+		Class<DTO> dtoClass = (Class<DTO>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+
+		BeanUtils.copyProperties(entity,dto);
+		return	dto;
 	}
 
 	@GetMapping("/{id}")
